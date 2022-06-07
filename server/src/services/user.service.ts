69600f15c554d03dd2201be31979eae294023f9c
Ouu Naja <nextchanupol@gmail.com>
2022-06-07 07:47:45 +0700

    changed findUser function

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,39 +1,43 @@
-import { DocumentDefinition, FilterQuery } from "mongoose";
+import mongoose, { DocumentDefinition, FilterQuery, LeanDocument } from "mongoose";
 import { omit } from "lodash";
 import UserModel, { UserDocument } from "../models/user.model";
 
 export async function createUser(
-  input: DocumentDefinition<
-    Omit<UserDocument, "createdAt" | "updatedAt" | "comparePassword">
-  >
+    input: DocumentDefinition<Omit<UserDocument, "createdAt" | "updatedAt" | "comparePassword">>
 ) {
-  try {
-    return await UserModel.create(input);
-  } catch (e: any) {
-    throw new Error(e);
-  }
+    try {
+        return await UserModel.create(input);
+    } catch (e: any) {
+        throw new Error(e);
+    }
 }
+
 export async function validatePassword({
-  email,
-  password,
-}: {
-  email: string;
-  password: string;
+                                           email,
+                                           password,
+                                       }: {
+    email: string;
+    password: string;
 }) {
-  const user = await UserModel.findOne({ email });
+    const user = await UserModel.findOne({ email });
 
-  if (!user) {
-    return false;
-  }
+    if (!user) {
+        return false;
+    }
 
-  const isValid = await user.comparePassword(password);
+    const isValid = await user.comparePassword(password);
 
-  if (!isValid) return false;
+    if (!isValid) return false;
 
-  return omit(user.toJSON(), "password");
-  
+    return omit(user.toJSON(), "password");
+
 }
 
-export async function findUser(query: FilterQuery<UserDocument>) {
-  return UserModel.findOne(query).lean();
+// export const findUser = async (query: FilterQuery<UserDocument>) => UserModel.findOne(query).lean();
+
+export const findUser = async (
+    query: FilterQuery<UserDocument>
+): Promise<LeanDocument<UserDocument & { _id: mongoose.Schema.Types.ObjectId }> | null> => {
+    return UserModel.findOne(query).lean();
 }
+

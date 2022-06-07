02076ff508d0cc897e6d87caef30b665f25d53a7
Meowmerry <meow3525@gmail.com>
2022-06-07 15:07:59 -0500

    add table for user management

---
+++ client/src/components/nav-bar/nav-bar.tsx
@@ -0,0 +1,12 @@
+
+
+import React, { useEffect, useState } from 'react'
+export const NavBar = () => {
+	console.log('list table')
+
+	return (
+		<div>
+            Menu Bar
+		</div>
+	)
+}
---
+++ client/src/pages/landing-page/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import React, { useEffect, useState } from 'react';
+import { NavBar } from '../../components/nav-bar/nav-bar';
+
+const LadingPage: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <NavBar />
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+export default LadingPage

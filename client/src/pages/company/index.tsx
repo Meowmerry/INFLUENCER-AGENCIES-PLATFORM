c3795d99dbaac8a0b5a7b2b234eea78c4b761270
Chayanit McCormick <72841865+chay-chay@users.noreply.github.com>
2022-06-08 18:33:43 -0400

    Created Company Landing page

--- client/src/pages/company/index.tsx
+++ client/src/pages/company/index.tsx
@@ -5,7 +5,7 @@ import men from '../../assets/images/inflencer.png';
 interface CompanyLandingProps { }
 
 const CompanyLanding: FunctionComponent<CompanyLandingProps> = (props) => {
-    const router = useRouter();
+    const router = useRouter();›
 
     return (
         // <div>Company Landing

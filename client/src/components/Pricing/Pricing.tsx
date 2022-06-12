2064986af5fd89c8b2b6f7da367a00783e29e943
Meowmerry <meow3525@gmail.com>
2022-06-12 14:17:04 -0500

    add pricing page

--- client/src/components/Pricing/Pricing.tsx
+++ client/src/components/Pricing/Pricing.tsx
@@ -46,7 +46,7 @@ const Pricing: FunctionComponent<PricingProps> = (props) => {
               <p className="text-[#4998DD] text-xs italic mt-1"> service fee </p>
               <div className="text-xl  text-[#222325] my-4 font-semibold">What Brands & Agency get:</div>
               <div className="text-left">
-                <ul className="text-base  text-[#222325] my-2 ml-16">
+                <ul className="text-base  text-[#222325] my-2 ml-8">
                   <li className="flex"> <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
                   </svg>Access to Community of Influencers</li>

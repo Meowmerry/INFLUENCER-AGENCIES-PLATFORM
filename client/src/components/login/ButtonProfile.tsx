602b2fea6618a772a5b2e5948bb902ad4b6e0ae8
Meowmerry <meow3525@gmail.com>
2022-06-21 21:13:05 -0500

    add profile

--- client/src/components/login/ButtonProfile.tsx
+++ client/src/components/login/ButtonProfile.tsx
@@ -13,70 +13,149 @@ export  const ButtonProfile: FunctionComponent<ButtonProfileProps> = ( props ) =
 
   return (
     <>
-      <Menu as="div" className="relative inline-block text-left">
-        <div>
-        
-            USER
-            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-   
-        </div>
-        
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
+     <div className="dropdown relative">
+        <button
+          className="
+            dropdown-toggle
+            px-6
+            py-2.5
+            bg-blue-600
+            text-white
+            font-medium
+            text-xs
+            leading-tight
+            uppercase
+            rounded
+            shadow-md
+            hover:bg-blue-700 hover:shadow-lg
+            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-blue-800 active:shadow-lg active:text-white
+            transition
+            duration-150
+            ease-in-out
+            flex
+            items-center
+            whitespace-nowrap
+          "
+          type="button"
+          id="dropdownMenuButton1ds"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
         >
-          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="py-1 ">
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                   
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm"
-                    )}
-                  >
-                   SETTING
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                   
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm"
-                    )}
-                  >
-                   CONTACT US
-                  </a>
-                )}
-              </Menu.Item>
-              <form method="POST" action="#">
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                    href="/signout"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm"
-                    )}
-                  >
-                      SIGN OUT
-                    </a>
-                  )}
-                </Menu.Item>
-              </form>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
+          USER
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="caret-down"
+            className="svg-inline--fa fa-caret-down w-2 ml-2"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path
+              fill="currentColor"
+              d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+            ></path>
+          </svg>
+        </button>
+        <ul
+          className="
+            dropdown-menu
+            min-w-max
+            absolute
+            hidden
+            bg-white
+            text-base
+            z-50
+            float-left
+            py-2
+            list-none
+            text-left
+            rounded-lg
+            shadow-lg
+            mt-1
+            m-0
+            bg-clip-padding
+            border-none
+          "
+          aria-labelledby="dropdownMenuButton1ds"
+        >
+          <li>
+            <a
+              className="
+                dropdown-item
+                text-sm
+                py-2
+                px-4
+                font-normal
+                block
+                w-full
+                whitespace-nowrap
+                bg-transparent
+                text-gray-700
+                hover:bg-gray-100
+              "
+              href="#"
+              >Action</a>
+          </li>
+          <li>
+            <a
+              className="
+                dropdown-item
+                text-sm
+                py-2
+                px-4
+                font-normal
+                block
+                w-full
+                whitespace-nowrap
+                bg-transparent
+                text-gray-700
+                hover:bg-gray-100
+              "
+              href="#"
+              >Another action</a>
+          </li>
+          <li>
+            <a
+              className="
+                dropdown-item
+                text-sm
+                py-2
+                px-4
+                font-normal
+                block
+                w-full
+                whitespace-nowrap
+                bg-transparent
+                text-gray-400
+                pointer-events-none
+              "
+              href="#"
+            >Something else here
+            </a>
+          </li>
+        </ul>
+      </div>
+    
     </>
   );
 }
+   {/* <div className="user-text">User </div>
+         <a
+              className="dropdown-toggle flex items-center hidden-arrow"
+              href="#"
+              id="dropdownMenuButton2"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                src="https://mdbootstrap.com/img/new/avatars/2.jpg"
+                className="rounded-full"
+                style={{ height: "50px", width: "50px" }}
+                alt=""
+                loading="lazy"
+              />
+        </a> */}
